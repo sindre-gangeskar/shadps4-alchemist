4e141f4399dc2bdbe21f0401ea5e7d5f07056a79
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2024-09-23 01:48:20 +0200

    bump version to v0.0.2-alpha.1

--- src/partials/VersionVisuals.jsx
+++ src/partials/VersionVisuals.jsx
@@ -1,7 +1,7 @@
 import '../css/VersionVisuals.css';
 function VersionVisuals() {
   return (
-    <p className="version">v0.0.2</p>
+    <p className="version">v0.0.2-alpha.1</p>
   )
 }
 export default VersionVisuals;
